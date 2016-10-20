ff05656fdea08a57227ee535fb43d10899684347
Guhan Muruganandam <gm114@duke.edu>
2016-10-20 01:11:55 -0400

    Filled in Turtle execution commands

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -1,55 +1,79 @@
 package model;
 
+import javafx.scene.image.ImageView;
 
 public class Turtle {
 	private double xpos;
 	private double ypos;
 	private boolean revealBoolean=true;
 	private boolean penBoolean=false;
+	private ImageView image;
+	private double angleNow;
 	
 	public Turtle(){
 		
 	}
 	/**
-	 * Addresses setting the co-ordinates of the turtl
-	 * Used for FORWARD,BACK,SETXY,HOME,CLEARSCREEN
+	 * Changes position of turtle
+	 * Used for FORWARD,BACK
+	 */
+	public void move(double vector){
+		double xmove=vector*(Math.sin(Math.toRadians(angleNow)));
+		double ymove=vector*(Math.cos(Math.toRadians(angleNow)));
+		xpos=xpos+xmove;
+		ypos=ypos+ymove;
+		image.setX(xpos);
+		image.setY(ypos);
+	}
+	/**
+	 * Sets position of turtle
+	 * Used for Home,ClearScreen
 	 */
 	public void setPosition(double x, double y){
-		//
+		image.setX(x);
+		image.setY(y);
+		xpos=x;
+		ypos=y;
 	}
 	
 	/**
 	 * Used for XCOR
 	 */
-	public void getPositionX(){
-		//
+	public double getPositionX(){
+		return xpos;
 	}
 	
 	/**
 	 * Used for YCOR
 	 */
-	public void getPositionY(){
-		//
+	public double getPositionY(){
+		return ypos;
 	}
 	
 	/**
 	 * Sets the angle of the turtle
-	 * Used for RIGHT,LEFT,SETHEADING,TOWARDS and all resets
+	 * Used for RIGHT,LEFT
 	 */
-	public void setOrientation(double angle){
-		//
-	} 
+	public void changeOrientation(double angle){
+		image.setRotate(angle);
+		angleNow=angle;
+	}
+	
 	/**
+	 * Sets the angle of the turtle
+	 * Used for RIGHT,LEFT,SETHEADING,TOWARD and all resets
+	 */
+	
+	public void setOrientation(double angle){
+		image.setRotate(360-angleNow);
+		angleNow=angle;
+		image.setRotate(angle);
+	}
+	 /**
 	 * Used for HEADING
 	 */
-	public void getOrientation(){
-		//
-	}
-	/**
-	 * Used for FORWARD,BACK
-	 */
-	public void moveDirection(double value){
-		
+	public double getOrientation(){
+		return angleNow;
 	}
 	
 	/**
