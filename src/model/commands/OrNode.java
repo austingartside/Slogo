569bb092b139dfc01863ab4faa6bfbada0b2fa1f
Austin <aeg36@duke.edu>
2016-10-22 18:57:09 -0400

    fixed minus and or nodes

--- src/model/commands/OrNode.java
+++ src/model/commands/OrNode.java
@@ -21,8 +21,9 @@ public class OrNode extends LogicCommand{
 		double test1=this.executeChild(FIRSTENTRY, control);
 		double test2=this.executeChild(SECONDENTRY, control);
 		// This format has to be used so that the return is a double and not a boolean.
-		if(test1!=ZERO || test2!=ZERO){return 1;}
-		else{return 0;}
+		return !(test1 == ZERO && test2 == ZERO) ? 1:0;
+//		if(test1!=ZERO || test2!=ZERO){return 1;}
+//		else{return 0;}
 	}
 
 }
