6a0052c7b28144c37871c2f9a5045cdedd3c6b87
Grant Costa <gac19@duke.edu>
2016-10-27 14:26:27 -0400

    added help button component

---
+++ src/View/HelpButton.java
@@ -0,0 +1,42 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class HelpButton {
+    
+    private Button help;
+    
+    public HelpButton(){
+        help = new Button("Help");
+        help.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(final ActionEvent ae){
+                displayHelp();
+            }
+        });
+        help.setMaxWidth(Double.MAX_VALUE);
+    }
+    
+    private void displayHelp(){
+        String path = System.getProperty("user.dir");
+        path += "/src/help.html";
+        WebView web = new WebView();
+        web.getEngine().load("file:///" + path);
+        Stage s = new Stage();
+        Scene scene = new Scene(web);
+        scene.setRoot(web);
+        s.setScene(scene);
+        s.show();
+    }
+    
+    public Node getView(){
+        return help;
+    }
+
+}
