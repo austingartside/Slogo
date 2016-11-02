6189d25e40b87f7b29c30b53bdd61b274101fb59
Guhan Muruganandam <gm114@duke.edu>
2016-11-01 20:15:09 -0400

    Backup point

--- src/model/TurtleController.java
+++ src/model/TurtleController.java
@@ -44,7 +44,6 @@ public class TurtleController {
 	
 	public void setTurtleArmy(){
 		Collection<Turtle> tempCollection=new ArrayList<Turtle>();
-		
 		Iterator<Double> tempIterator= myAskList.iterator();
     	while(tempIterator.hasNext()){
     		Double i=tempIterator.next();
